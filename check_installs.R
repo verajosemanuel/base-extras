26cbb8d188ef2b80aac2256e301b785e1aa5ef23
Jose Manuel Vera <vera.josemanuel@gmail.com>
2018-06-01 11:47:58 +0200

    Create check_installs.R

---
+++ check_installs.R
@@ -0,0 +1,5 @@
+packages<-c("bigrquery", "cartography", "choroplethr", "cr17", "d3heatmap", "d3Tree", "alphahull", "datamaps", "directlabels", "dygraphs")
+check.packages(packages)
+
+packages<-c("egg", "gdaUtils", "geofacet", "geonames", "geoR", "geosphere", "ggalt", "ggbeeswarm", "ggcorrplot", "ggedit", "ggforce")
+check.packages(packages)
